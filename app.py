aac25ea48c5f33d1bdcd05da3bc86ec92de9b8e2
Wander <Wandervdwal@gmail.com>
2019-08-07 00:59:24 +0200

    Improve

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gpt_2_simple as gpt2
 
-sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess)
+model_name = "117M"
+gpt2.download_gpt2(model_name=model_name)
 
-gpt2.finetune(sess,'encoding_examples.txt',model_name=model_name,steps=7000)
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,'encoding.txt',model_name=model_name,steps=6000)
